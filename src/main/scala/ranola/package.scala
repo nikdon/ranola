bea0c76214737c03c067d8bc5c5493cb9858bfb0
nd <nd.startup@gmail.com>
2015-04-17 16:46:04 +0300

    add type bounds

--- src/main/scala/ranola/package.scala
+++ src/main/scala/ranola/package.scala
@@ -6,8 +6,8 @@ package object ranola {
   type AnyMatrix = Matrix[_]
 
   // Multiplications
-  type OpMulMatrixDenseMatrix[M] = OpMulMatrix.Impl2[M, DenseMatrix[Double], DenseMatrix[Double]]
-  type OpMulMatrixDenseVector[M] = OpMulMatrix.Impl2[M, DenseVector[Double], DenseVector[Double]]
-  type OpMulDenseMatrixMatrix[M] = OpMulMatrix.Impl2[DenseMatrix[Double], M, DenseMatrix[Double]]
+  type OpMulMatrixDenseMatrix[M <: AnyMatrix] = OpMulMatrix.Impl2[M, DenseMatrix[Double], DenseMatrix[Double]]
+  type OpMulMatrixDenseVector[M <: AnyMatrix] = OpMulMatrix.Impl2[M, DenseVector[Double], DenseVector[Double]]
+  type OpMulDenseMatrixMatrix[M <: AnyMatrix] = OpMulMatrix.Impl2[DenseMatrix[Double], M, DenseMatrix[Double]]
 
 }
