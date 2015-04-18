e5a06169c1f105c2914d85f6ea71dea383616e65
nd <nd.startup@gmail.com>
2015-04-18 23:23:16 +0300

    clean up in test helpers

--- src/test/scala/ranola/TestHelpers.scala
+++ src/test/scala/ranola/TestHelpers.scala
@@ -7,12 +7,12 @@ import org.scalatest._
 
 object TestHelpers extends FunSuite with Matchers with DoubleImplicits {
 
-  def vectorsNearlyEqual(A: DenseVector[Double], B: DenseVector[Double], threshold: Double = 1E-6) {
+  def vectorsNearlyEqual(A: DenseVector[Double], B: DenseVector[Double], threshold: Double = 1E-6): Unit = {
     for(i <- 0 until A.length)
       A(i) should be (B(i) +- threshold)
   }
 
-  def matricesNearlyEqual(A: DenseMatrix[Double], B: DenseMatrix[Double], threshold: Double = 1E-6) {
+  def matricesNearlyEqual(A: DenseMatrix[Double], B: DenseMatrix[Double], threshold: Double = 1E-6): Unit = {
     for(i <- 0 until A.rows; j <- 0 until A.cols)
       A(i,j) should be (B(i, j) +- threshold)
   }
